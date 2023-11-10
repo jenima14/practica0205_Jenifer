0a0ee07bc06bca2f16d29fa7421decb424c56626
jenima14 <jmarticarr2@educacion.navarra.es>
2023-11-10 20:42:53 +0100

    ejercicio_04

---
+++ ejercicio_04.py
@@ -0,0 +1,12 @@
+datos = {}
+datos["Nombre"] = input("Escribe el nombre: ")
+print("Diccionario actualizado:", datos)
+datos["Edad"] = input("Escribe la edad: ")
+print("Diccionario actualizado:", datos)
+datos["Sexo"] = input("Escribe el sexo: ")
+print("Diccionario actualizado:", datos)
+datos["Teléfono"] = input("Escribe el número de teléfono: ")
+print("Diccionario actualizado:", datos)
+datos["Dirección"] = input("Escribe la dirección: ")
+print("Diccionario final:", datos)
+input()
